e0e6d14270e587f0118e676670184994bca2a311
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-07-23 18:14:58 -0400

    commit tree 0b0c50b1d22c0e298bac6133aab37f7ba9cd118f

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// code your solution here
+// function superbowlWin(array) {
+
+//     let football = array.find(element => element.result === "W");
+
+//     if(!!football) {
+//      return football.year;
+//     }
+
+// }
+
+////////////////////////
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+function superbowlWin(arr) {
+  let finder = arr.find((element) => element.result === "W");
+
+  if (finder) {
+    return finder.year;
+  }
+}
